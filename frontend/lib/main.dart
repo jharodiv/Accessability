5c50e271138cf3344bc27e18c7407846a7c4f8ee
Jem Centino <138832632+Kuhwu@users.noreply.github.com>
2025-01-14 08:28:19 +0800

    Minimal Changes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/accessability/screens/splashscreen.dart';
 //import 'package:frontend/accessability/screens/splashscreen.dart';
 import 'package:frontend/accessability/widgets/homepagewidgets/topwidgets.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Topwidgets(),
+    home: Splashscreen(),
   ));
 }

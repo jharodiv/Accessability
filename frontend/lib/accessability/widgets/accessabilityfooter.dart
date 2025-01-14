1521054dadad2de2e863756e6a0e4506d06bfacd
Jem Centino <138832632+Kuhwu@users.noreply.github.com>
2025-01-14 13:37:33 +0800

    Frontend Update
    [1] Bottom Navigation UI

--- frontend/lib/accessability/widgets/accessabilityfooter.dart
+++ frontend/lib/accessability/widgets/accessabilityfooter.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
 
-class Accessabilityfooter extends StatelessWidget {
+class Accessabilityfooter extends StatefulWidget {
   const Accessabilityfooter({super.key});
 
   @override
+  State<Accessabilityfooter> createState() => AccessabilityfooterState();
+}
+
+class AccessabilityfooterState extends State<Accessabilityfooter> {
+  int currentIndex = 0;
+
+  final List<Widget> navs = [
+    const Center(
+      child: Text('Location'),
+    ),
+    const Center(
+      child: Text('You'),
+    ),
+    const Center(
+      child: Text('Safety'),
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: navs[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.location_on),
+              label: 'Location',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'You',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.security),
+              label: 'Safety',
+            ),
+          ]),
+    );
   }
 }

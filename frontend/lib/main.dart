756f5fa8ec2f262f49ad6def63274ff02547a4bc
Renek <angjaydeebryann@gmail.com>
2025-01-19 11:24:24 +0800

    firebase initialized

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:frontend/accessability/router/app_router.dart';
+import 'package:frontend/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 

cda4505433d23d162568df7174a873fa855b81e2
Renek <angjaydeebryann@gmail.com>
2025-01-19 18:41:47 +0800

    added register function in auth service

--- frontend/lib/accessability/firebaseServices/auth_service.dart
+++ frontend/lib/accessability/firebaseServices/auth_service.dart
@@ -3,6 +3,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  //Register
+  Future<UserCredential> signUpWithEmailAndPassword(
+      String email, password) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
   // Login
   Future<UserCredential> signInWithEmailPassword(String email, password) async {
     try {

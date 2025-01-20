c0ea85aaa11b29bce7e6f37396e1b84f707530fe
Renek <angjaydeebryann@gmail.com>
2025-01-21 05:34:41 +0800

    added user tile padding

--- frontend/lib/accessability/presentation/widgets/chatWidgets/chat_users_tile.dart
+++ frontend/lib/accessability/presentation/widgets/chatWidgets/chat_users_tile.dart
@@ -19,6 +19,8 @@ class ChatUsersTile extends StatelessWidget {
             color: Theme.of(context).colorScheme.secondary,
             borderRadius: BorderRadius.circular(12),
           ),
+          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+          padding: EdgeInsets.all(20),
           child: Row(
             children: [
               const Icon(Icons.person),

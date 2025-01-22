5f654c032544d910b11c729b44d55a2b9a4db05f
Renek <angjaydeebryann@gmail.com>
2025-01-22 16:13:37 +0800

    message model made

--- frontend/lib/accessability/firebaseServices/chat/chat_service.dart
+++ frontend/lib/accessability/firebaseServices/chat/chat_service.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:frontend/accessability/firebaseServices/models/message.dart';
 
 class ChatService {
   final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Stream<List<Map<String, dynamic>>> getUserStream() {
     return firebaseFirestore.collection("Users").snapshots().map((snapshot) {
@@ -11,5 +14,28 @@ class ChatService {
         return user;
       }).toList();
     });
+  }
+
+  Future<void> sendMessage(String receiverID, message) async {
+    final String currentUserID = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
+
+    Message newMessage = Message(
+      senderID: currentUserID, 
+      senderEmail: currentUserEmail, 
+      receiverID: receiverID, 
+      message: message, 
+      timestamp: timestamp);
+
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    await firebaseFirestore
+      .collection('chat_rooms')
+      .doc(chatRoomID)
+      .collection('messages')
+      .add(newMessage.toMap());
   }
 }

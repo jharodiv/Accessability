1c281fe454189174e7f420b40686dafdae2b202b
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-02-04 13:18:32 +0800

    added send verification code route

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -10,6 +10,9 @@ router.post('/signup', upload.single('image'), authController.signup);
 // ** Login route
 router.post('/login', authController.login);
 
+// ** Send verification route
+router.post('/sendVerificationCode', authController.sendVerificationCode);
+
 // ** Verify route
 router.post('/verifyCode', authController.verifyCode);
 router.post('/resendCode', authController.resendVerificationCode);

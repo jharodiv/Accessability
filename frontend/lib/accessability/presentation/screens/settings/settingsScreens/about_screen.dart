77f36d2e6b6658fc5b3ddec069bae3e1c8564509
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-03-11 10:54:37 +0800

    added about page filipino language and redesign again

--- frontend/lib/accessability/presentation/screens/settings/settingsScreens/about_screen.dart
+++ frontend/lib/accessability/presentation/screens/settings/settingsScreens/about_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:AccessAbility/accessability/themes/theme_provider.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
@@ -24,17 +25,14 @@ class AboutScreen extends StatelessWidget {
             ],
           ),
           child: AppBar(
-            elevation: 0,
             leading: IconButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
+              onPressed: () => Navigator.of(context).pop(),
               icon: const Icon(Icons.arrow_back),
               color: const Color(0xFF6750A4),
             ),
-            title: const Text(
-              'About',
-              style: TextStyle(fontWeight: FontWeight.bold),
+            title: Text(
+              'aboutTitle'.tr(),
+              style: const TextStyle(fontWeight: FontWeight.bold),
             ),
             centerTitle: true,
             backgroundColor: Colors.transparent,
@@ -46,15 +44,18 @@ class AboutScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'What is Accessability?',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: isDarkMode ? Colors.white : Colors.black87,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'aboutHeader'.tr(),
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                  color: isDarkMode ? Colors.white : Colors.black87,
+                ),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 10),
             Container(
               padding: const EdgeInsets.all(15),
               decoration: BoxDecoration(
@@ -68,17 +69,21 @@ class AboutScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              child: Text(
-                "Accessability is an innovative GPS navigation application created as part of a student capstone project to support persons with disabilities (PWD). Designed with inclusivity and accessibility at its core, Accessability aims to empower individuals by providing reliable, user-friendly navigation tools that enhance mobility and independence. Whether navigating city streets, public transportation, or unfamiliar locations, this app is tailored to meet the unique needs of PWD, offering a seamless and supportive experience.\n\nDeveloped by a dedicated student passionate about creating meaningful solutions, Accessability integrates advanced features such as voice-guided navigation, step-free route suggestions, and real-time accessibility updates for paths, buildings, and transit systems. The app's customizable interface ensures it caters to a diverse range of disabilities, making it a versatile and practical tool for everyday use. As a capstone project, Accessability reflects a commitment to leveraging technology to address real-world challenges and improve lives.\n\nAccessability is more than just a navigation tool—it’s a testament to the power of innovation and determination in creating a more inclusive world. By prioritizing safety, accessibility, and convenience, this project aims to redefine mobility for PWD, demonstrating how student-driven initiatives can have a lasting, positive impact.",
-                style: TextStyle(
-                  fontSize: 14,
-                  color: isDarkMode ? Colors.white : Colors.black87,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  'aboutDescription'.tr(),
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: isDarkMode ? Colors.white : Colors.black87,
+                  ),
                 ),
               ),
             ),
             const SizedBox(height: 20),
             Text(
-              'Proponents',
+              'aboutProponents'.tr(),
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
@@ -131,7 +136,7 @@ class AboutScreen extends StatelessWidget {
                       "role": "System Integrator",
                       "email": "lafa.flores.up@phinmaed.com",
                       "image": "assets/images/others/kian.png"
-                    },
+                    }
                   ])
                     Padding(
                       padding: const EdgeInsets.symmetric(vertical: 10),
@@ -150,21 +155,27 @@ class AboutScreen extends StatelessWidget {
                                 style: TextStyle(
                                   fontSize: 16,
                                   fontWeight: FontWeight.bold,
-                                  color: isDarkMode ? Colors.white : Colors.black87,
+                                  color: isDarkMode
+                                      ? Colors.white
+                                      : Colors.black87,
                                 ),
                               ),
                               Text(
-                                '${proponent['role']}',
+                                proponent['role']!,
                                 style: TextStyle(
                                   fontSize: 14,
-                                  color: isDarkMode ? Colors.white : Colors.black87,
+                                  color: isDarkMode
+                                      ? Colors.white
+                                      : Colors.black87,
                                 ),
                               ),
                               Text(
-                                '${proponent['email']}',
+                                proponent['email']!,
                                 style: TextStyle(
                                   fontSize: 14,
-                                  color: isDarkMode ? Colors.white : Colors.black87,
+                                  color: isDarkMode
+                                      ? Colors.white
+                                      : Colors.black87,
                                 ),
                               ),
                             ],

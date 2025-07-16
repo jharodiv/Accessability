69106bb8d6a9c8bafbc79fed31bbfc611bc4a439
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-07-16 22:06:29 +0800

    create error widget

---
+++ frontend/lib/accessability/presentation/widgets/errorWidget/error_display_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ErrorDisplayWidget extends StatelessWidget {
+  final String title;
+  final String message;
+
+  const ErrorDisplayWidget({
+    Key? key,
+    required this.title,
+    required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icon/app_icon.png',
+              width: 100,
+              height: 100,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              message,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

96a847b2e4a6905fed120acffe1db668f172cc21
Christer Dale Montilla Reyes <rchristerdale@gmail.com>
2025-08-28 09:13:54 +0800

    fix: remove write issue permision and delete release tag

--- release.config.js
+++ release.config.js
@@ -3,14 +3,22 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github'
+    [
+      '@semantic-release/github',
+      {
+        // Disable creating issues on fail
+        successComment: false,
+        failComment: false,
+        failTitle: false
+      }
+    ]
   ],
   preset: 'conventionalcommits',
   releaseRules: [
     { type: 'fix', release: 'patch' },
     { type: 'feat', release: 'minor' },
     { type: 'chore', release: 'minor' },
-    { type: 'trigger', release: 'major' }, // your custom keyword
+    { type: 'trigger', release: 'major' } // your custom keyword
   ],
   parserOpts: {
     noteKeywords: ['BREAKING CHANGE', 'trigger']
